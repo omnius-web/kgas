c5c444efebf98598b6f043cc6af82656e99312d6
omnius <gs5m3011@gmail.com>
2020-08-11 13:34:08 +0900

    OM_Mail Class Set

---
+++ Mail/Mail.php
@@ -0,0 +1,18 @@
+<?php
+namespace Mail;
+/**
+ * @author OMNIUS
+ * @author Pigweb
+ * @copyright 2020 OMNIUS
+ * @copyright 2020 Pigweb
+ */
+class OM_Mail
+{
+
+  function __construct(argument)
+  {
+    // code...
+  }
+}
+
+?>
